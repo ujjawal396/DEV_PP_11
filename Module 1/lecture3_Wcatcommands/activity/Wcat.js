60ee2205ee62aef57f11623f7852b84f357a8636
ujjawaljha <ujjawaljha08@gmail.com>
2021-05-08 14:39:40 +0530

    wcat activity started

--- Module 1/lecture3_Wcatcommands/activity/Wcat.js
+++ Module 1/lecture3_Wcatcommands/activity/Wcat.js
@@ -1,50 +1,21 @@
-const { getFilesData, applySFlag, applyBFlag, applyNFlag } = require("./util");
-let contents = process.argv.slice(2);
-// console.log(contents);
+let fs =require("fs");
+//const { argv } = require("node:process");
+//console.log(process);
+//console.log(process.argv);
 
-const flags = [];
-const files = [];
+let contents=process.argv.slice(2);
+const flag=[];
+const files=[];
 
-for (let i = 0; i < contents.length; i++) {
-    // "-s" , "-b" , "f1.txt" , "f2.txt" , "-n"
-  if (contents[i].startsWith("-")) {
-    flags.push(contents[i]);
-  } else {
+for(let i=0;i<contents.length;i++){
+  if(contents[i].startsWith("-")){
+    flag.push(contents[i]);
+
+  }else{
     files.push(contents[i]);
+
   }
 }
 
-// -s -b -n
-// f1.txt // f2.txt
-
-let filesData = getFilesData(files);
-
-if (flags.includes("-s")) {
-  // filesData updated if s flag is present !
-  filesData = applySFlag(filesData);
-}
-
-// console.log(filesData);
-
-// when both -b and -n flags are present
-if (flags.includes("-b") && flags.includes("-n")) {
-  if (flags.indexOf("-b") < flags.indexOf("-n")) {
-    // apply b flag
-    filesData = applyBFlag(filesData);
-  } else {
-    // apply n flag
-    filesData = applyNFlag(filesData);
-  }
-}
-// only -b flag is present
-else if (flags.includes("-b")) {
-  // apply b flag
-  filesData = applyBFlag(filesData);
-}
-// only -n flag is present
-else if (flags.includes("-n")) {
-  // apply n flag
-  filesData = applyNFlag(filesData);
-}
-
-console.log(filesData);
+console.log(flag);
+console.log(files);
