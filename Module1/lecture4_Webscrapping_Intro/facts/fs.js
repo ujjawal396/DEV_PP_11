25a3bcaf65323d0bd385062f4e7dc6a2ab900d3b
ujjawaljha <ujjawaljha08@gmail.com>
2021-05-10 15:02:52 +0530

    selector concepts

--- Module1/lecture4_Webscrapping_Intro/facts/fs.js
+++ Module1/lecture4_Webscrapping_Intro/facts/fs.js
@@ -1,5 +1,4 @@
 // In DEV_PP11 folder
-//initial steps
 // npm init -y
 // npm install cheerio
 
@@ -18,4 +17,32 @@ let myDocument = cheerio.load(htmlKaData);
 // console.log(myDocument);
 let h1KaData = myDocument("h1").text();
 // console.log(h1Element); element => cheerio => object form me data
- console.log(h1KaData);
+// console.log(h1KaData);
+
+// let secondPTag = myDocument("p")["1"];
+
+// console.log(secondPTag.text());
+
+// console.log(myDocument(secondPTag).text());
+
+// Selectors
+// console.log( myDocument("ul p").text() ); // it will give all the p tags inside ul
+
+// a tag
+// console.log(myDocument("a").text());
+// you will get all a tags inside li
+// console.log(myDocument("ul li a").text());
+
+// only direct child !!
+// console.log(myDocument("ul>a").text());
+
+
+// classes and ids
+// dot
+// console.log( myDocument(".inside").text() );
+console.log( myDocument(".inside.main").text()  );
+
+
+// ids => 
+// #
+console.log( myDocument("#main-heading").text() );
