f3fea95f53e1d65008a3c7ec863eee3b69e582ef
ujjawaljha <ujjawaljha08@gmail.com>
2021-05-17 17:32:37 +0530

    async

--- Module1/lecture6_Async/facts/asyncParallel.js
+++ Module1/lecture6_Async/facts/asyncParallel.js
@@ -8,10 +8,12 @@ fs.readFile("./f1.txt" ,function(error,data){
     console.log(data+ "");
 
 })
+
 fs.readFile("./f2.txt" ,function(error,data){
     console.log(data+ "");
     
 })
+
 
 fs.readFile("./f3.txt" ,function(error,data){
     console.log(data+ "");
--- Module1/lecture6_Async/facts/sync.js
+++ Module1/lecture6_Async/facts/sync.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 
 console.log("start");
 
-let f1KaContent = fs.readFileSync("./f1.txt"); // 100gb file
-console.log(f1KaContent+"");
+let f1KaContent = fs.readFileSync("./f1.txt" ); // 100gb file
+console.log(f1KaContent + "");
 
 console.log("end");
 
