8334f1aebc14c919c8a7110ff7fca4f76103a853
ujjawaljha <ujjawaljha08@gmail.com>
2021-05-17 19:46:57 +0530

    js concepts 1

---
+++ Module1/JS concepts/fun.js
@@ -0,0 +1,16 @@
+// console.log(a);
+// console.log(fun);
+// fun();
+
+// var a = 20;
+// var fun = function(){
+//     console.log("Fun Says Hi !!!");
+// }
+console.log(a);
+console.log(fun);
+fun();
+
+let a=20;
+let fun=function(){
+    console.log("Fun says hi!!");
+}
