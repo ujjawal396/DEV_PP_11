1847a4319ed871d9441f6a2f9c738a1550459192
ujjawaljha <ujjawaljha08@gmail.com>
2021-05-18 14:49:58 +0530

    commited

--- Module1/JS concepts/hoisting.js
+++ Module1/JS concepts/hoisting.js
@@ -17,14 +17,14 @@
 
 
 // undefined
-console.log(a);
-var a = "Steve";
-console.log(a);
+// console.log(a);
+// var a = "Steve";
+// console.log(a);
 
 // not defined
-console.log(c);
+//console.log(c);
 
 // cannot access a before initialization
-console.log(b);
+ console.log(b);
 let b = "Steve";
 console.log(b);
