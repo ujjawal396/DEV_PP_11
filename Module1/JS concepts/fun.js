8a224a7b351e017c5d41babb8bc6fc45c5dc5359
ujjawaljha <ujjawaljha08@gmail.com>
2021-05-27 14:19:39 +0530

    scope

--- Module1/JS concepts/fun.js
+++ Module1/JS concepts/fun.js
@@ -1,14 +1,14 @@
 // console.log(a);
 // console.log(fun);
-// fun();
+//  fun();
 
 // var a = 20;
 // var fun = function(){
 //     console.log("Fun Says Hi !!!");
 // }
-console.log(a);
-console.log(fun);
-fun();
+// console.log(a);
+// console.log(fun);
+// fun();
 
 let a=20;
 let fun=function(){
