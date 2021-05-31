5a9d3f3d4d184b45ff77589e8e300b0014ebba1d
ujjawaljha <ujjawaljha08@gmail.com>
2021-05-31 15:34:48 +0530

    promise chaining

--- Module1/lecture7_promises/nFiles/PromisesParallel.js
+++ Module1/lecture7_promises/nFiles/PromisesParallel.js
@@ -4,8 +4,9 @@ let files=["../f1.txt","../f2.txt","../f3.txt"];
 
 
 for(let i=0;i<files.length;i++){
-    let fileskaPromises=fs.promises.readFile(files[i],function(data){
+    let fileskaPromises=fs.promises.readFile(files[i]);
+    fileskaPromises.then(function(data){
         console.log(data+"");
     })
 
-}
+};
