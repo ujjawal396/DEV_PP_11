74125fc631781929a61b4a97e79cba2252c30ed6
ujjawaljha <ujjawaljha08@gmail.com>
2021-06-14 21:43:04 +0530

    css added

---
+++ Module2/ticket app/activity/script.js
@@ -0,0 +1,18 @@
+let allFilters=document.querySelectorAll(".filter");
+let ticketsContainer=document.querySelector(".ticket-container");
+
+
+for(let i=0;i<allFilters.length;i++){
+    allFilters[i]=addEventListener("click",selectFilter);
+}
+
+function selectFilter(e){
+    let filterSelected=e.target.classList[1];
+
+    if(ticketsContainer.classList.length>1){
+        ticketsContainer.classList.remove(ticketsContainer.classList[1]);
+
+    }
+    ticketsContainer.classList.add(filterSelected);
+
+}
