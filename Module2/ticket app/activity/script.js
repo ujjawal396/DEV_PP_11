8fa04797ee4d196116a177852b282e62399c6814
ujjawaljha <ujjawaljha08@gmail.com>
2021-06-15 22:13:11 +0530

    css advance

--- Module2/ticket app/activity/script.js
+++ Module2/ticket app/activity/script.js
@@ -1,6 +1,16 @@
 let allFilters=document.querySelectorAll(".filter");
 let ticketsContainer=document.querySelector(".ticket-container");
 
+let openModal=document.querySelector(".open-modal");
+let closeModal=document.querySelector(".close-modal");
+
+let ticketModalOpen=false;
+
+
+
+openModal.addEventListener("click",openTicketModal);
+closeModal.addEventListener("click",closeTicketModal);
+
 
 for(let i=0;i<allFilters.length;i++){
     allFilters[i]=addEventListener("click",selectFilter);
@@ -16,4 +26,36 @@ function selectFilter(e){
     }
     ticketsContainer.classList.add(filterSelected);
 
+}
+
+function openTicketModal(e){
+    if(ticketModalOpen){
+        return;
+    }
+
+
+    let ticketModal=document.createElement("div");
+    ticketModal.classList.add("ticket-modal");
+    ticketModal.innerHTML=  `<div class="ticket-text" contentEditable="true">Enter your text</div>
+    <div class="ticket-filters">
+        <div class="ticket-filter red"></div>
+        <div class="ticket-filter blue"></div>
+        <div class="ticket-filter green"></div>
+        <div class="ticket-filter yellow"></div>
+        <div class="ticket-filter black"></div>  
+    </div>`;
+
+    document.querySelector("body").append(ticketModal);
+
+    ticketModalOpen=true;
+}
+
+
+
+function closeTicketModal(e){
+
+if(ticketModalOpen){
+    document.querySelector(".ticket-modal").remove();
+    ticketModalOpen=false;
+}
 }
