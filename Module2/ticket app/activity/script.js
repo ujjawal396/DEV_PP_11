3be937150c865d2cc257d8ef4b1966b64d811cc9
ujjawaljha <ujjawaljha08@gmail.com>
2021-06-16 20:50:30 +0530

    ticket header filter challenge

--- Module2/ticket app/activity/script.js
+++ Module2/ticket app/activity/script.js
@@ -1,107 +1,98 @@
-let allFilters=document.querySelectorAll(".filter");
-let ticketsContainer=document.querySelector(".ticket-container");
-
-let openModal=document.querySelector(".open-modal");
-let closeModal=document.querySelector(".close-modal");
-
-let ticketModalOpen=false;
+let allFilters = document.querySelectorAll(".filter");
+let openModal = document.querySelector(".open-modal");
+let closeModal = document.querySelector(".close-modal");
+let ticketModalOpen = false;
 let isTextTyped = false;
 
 
-openModal.addEventListener("click",openTicketModal);
-closeModal.addEventListener("click",closeTicketModal);
 
 
-for(let i=0;i<allFilters.length;i++){
-    allFilters[i]=addEventListener("click",selectFilter);
+
+for (let i = 0; i < allFilters.length; i++) {
+  allFilters[i].addEventListener("click", selectFilter);
 }
 
-function selectFilter(e){
-    console.log(e);
-    let filterSelected=e.target.classList[1];
+openModal.addEventListener("click", openTicketModal);
+closeModal.addEventListener("click", closeTicketModal);
 
-    if(ticketsContainer.classList.length>1){
-        ticketsContainer.classList.remove(ticketsContainer.classList[1]);
-
+function selectFilter(e) {
+  if(e.target.classList.contains("active-filter")){
+    // ticket append are on basis of some filter
+    e.target.classList.remove("active-filter");
+    // append all tickets
+    ticketsContainer.innerHTML = "";
+    loadTickets();
+  }
+  else{
+    if(document.querySelector(".active-filter")){
+      document.querySelector(".active-filter").classList.remove("active-filter");
     }
-    ticketsContainer.classList.add(filterSelected);
-
+    e.target.classList.add("active-filter");
+    ticketsContainer.innerHTML = "";
+    let filterClicked = e.target.classList[1];
+    loadSelectedTickets(filterClicked);
+  }
 }
 
-function openTicketModal(e){
-    if(ticketModalOpen){
-        return;
-    }
-
-
-    let ticketModal=document.createElement("div");
-    ticketModal.classList.add("ticket-modal");
-    ticketModal.innerHTML=  `<div class="ticket-text" contentEditable="true" spellcheck="false">Enter your text</div>
+function openTicketModal(e) {
+  if (ticketModalOpen) {
+    return;
+  }
+  let ticketModal = document.createElement("div");
+  ticketModal.classList.add("ticket-modal");
+  ticketModal.innerHTML = `<div class="ticket-text" contentEditable="true" spellcheck="false">Enter Your Text !</div>
     <div class="ticket-filters">
         <div class="ticket-filter red selected-filter"></div>
         <div class="ticket-filter blue"></div>
         <div class="ticket-filter green"></div>
         <div class="ticket-filter yellow"></div>
-        <div class="ticket-filter black"></div>  
+        <div class="ticket-filter black"></div>
     </div>`;
+  document.querySelector("body").append(ticketModal);
+  ticketModalOpen = true;
+  isTextTyped = false;
 
-    document.querySelector("body").append(ticketModal);
+  let ticketTextDiv = ticketModal.querySelector(".ticket-text");
+  ticketTextDiv.addEventListener("keypress", handleKeyPress);
 
-    ticketModalOpen=true;
-    let ticketTextDiv =ticketModal.querySelector(".ticket-text");
-    ticketTextDiv.addEventListener("keypress", handleKeyPress);  
-    
-    
-    let ticketFilters=ticketModal.querySelectorAll(".ticket-filter");
-
-    for(let i=0;i<ticketFilters.length;i++){
-
-        ticketFilters[i].addEventListener("click",function(e){
-
-            if(e.target.classList.contains("selected-filter")){
-                return;
-            }
-
-            document.querySelector(".selected-filter").classList.remove("selected-filter");
-            e.target.classList.add("selected-filter");
-        });
-        
-    }
-
+  let ticketFilters = ticketModal.querySelectorAll(".ticket-filter");
+  for (let i = 0; i < ticketFilters.length; i++) {
+    ticketFilters[i].addEventListener("click", function (e) {
+      if (e.target.classList.contains("selected-filter")) {
+        return;
+      }
+      document
+        .querySelector(".selected-filter")
+        .classList.remove("selected-filter");
+      e.target.classList.add("selected-filter");
+    });
+  }
 }
 
-
-
-function closeTicketModal(e){
-
-if(ticketModalOpen){
+function closeTicketModal(e) {
+  if (ticketModalOpen) {
     document.querySelector(".ticket-modal").remove();
-    ticketModalOpen=false;
-
-    isTextTyped=false;
-   }
+    ticketModalOpen = false;
+    isTextTyped = false;
+  }
 }
 
+function handleKeyPress(e) {
+  if (e.key == "Enter" && isTextTyped && e.target.textContent) {
+    let filterSelected = document.querySelector(".selected-filter").classList[1];
+    let ticketId = uuid();
+    let ticketInfoObject = {
+      ticketFilter: filterSelected,
+      ticketValue: e.target.textContent,
+      ticketId : ticketId
+    };
+    appendTicket(ticketInfoObject);
+    closeModal.click();
+    saveTicketToDB(ticketInfoObject);
+  }
 
-function handleKeyPress(e){
-
-    if(!isTextTyped){
-        isTextTyped=true;
-        e.target.textContent="";
-    }
+  if(!isTextTyped) {
+    isTextTyped = true;
+    e.target.textContent = "";
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
