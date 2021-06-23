fe0e56159aeaba003543b8726252fea4a6ddbc4b
ujjawaljha <ujjawaljha08@gmail.com>
2021-06-23 22:03:47 +0530

    undo added

--- Module2/whiteBoard/activity/script.js
+++ Module2/whiteBoard/activity/script.js
@@ -13,3 +13,49 @@ window.addEventListener("resize",function(){
 // a context object which provides fun for 2d drawing
 
 let ctx=canvas.getContext("2d");
+
+
+let linesDb=[];
+let isPenDown=false;
+let line=[];
+
+
+canvas.addEventListener("mousedown",function(e){
+    isPenDown=true;
+    let x=e.clientX;
+    let y=e.clientY-100;
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+
+    let pointObject={
+        x:x ,
+        y:y ,
+        type :"md"
+    }
+    line.push(pointObject);
+})
+canvas.addEventListener("mousemove",function(e){
+    if(isPenDown){
+    let x=e.clientX;
+    let y=e.clientY-100;
+    ctx.lineTo(x,y);
+    ctx.stroke();
+
+    }
+    let pointObject={
+        x:x ,
+        y:y ,
+        type :"mm"
+    }
+    line.push(pointObject);
+})
+canvas.addEventListener("mouseup",function(e){
+    isPenDown=false;
+
+    linesDb.push(line);
+    line=[];
+
+    console.log(linesDb)
+
+})
+
---
+++ Module2/whiteBoard/activity/tool.js
@@ -0,0 +1,30 @@
+let undo=document.querySelector("undo");
+
+undo.addEventListener("click",undoLine);
+
+
+function undoLine(){
+    linesDb.pop();
+
+    //clear canvas
+    ctx.clearRect(0,0,canvas.Width,canvas.height);
+
+    drawLinesFromDB();
+
+}
+function drawLinesFromDB(){
+    for(let i=0;i<linesDb.length;i++){
+        let line=linesDb[i];
+        for(let i=0;i<line.length;i++){
+            let pointObject=line[i];
+            if(pointObject.type=="md"){
+                ctx.beginPath();
+                ctx.moveTo(pointObject.x , pointObject.y);
+            }
+            else{
+                ctx.lineTo(pointObject.x , pointObject.y);
+                ctx.stroke();
+            }
+        }
+    }
+}
