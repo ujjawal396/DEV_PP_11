f9f503103f9ea8ea6b8140f94437cd12fc2bb603
ujjawaljha <ujjawaljha08@gmail.com>
2021-06-29 18:47:32 +0530

    canvas download as image

--- Module2/whiteBoard/activity/img.js
+++ Module2/whiteBoard/activity/img.js
@@ -1,27 +1,22 @@
-let photoDiv=document.querySelector("#photo");
-
-let photoUploadInput=document.querySelector("#photo-upload");
-
+let photoDiv = document.querySelector("#photo");
+let photoUploadInput = document.querySelector("#photo-upload");
 let downloadDiv = document.querySelector("#download");
 
-
-
-photoDiv.addEventListener("click",function(){
-    photoUploadInput.click();
+photoDiv.addEventListener("click", function () {
+  photoUploadInput.click();
 });
 
-
-
-photoUploadInput.addEventListener("change",function(event){
-    console.log(event);
-    let fileObj=event.target.files[0];
-    console.log(fileObj);
-    let filepath=URL.createObjectURL(fileObj);
-    let img=document.createElement("img");
-    img.setAttribute("src",filepath);
-    img.classList.add("sticky-image");
-    addSticky(img);
+photoUploadInput.addEventListener("change", function (event) {
+  console.log(event);
+  let fileObj = event.target.files[0];
+  console.log(fileObj);
+  let filePath = URL.createObjectURL(fileObj, { type: "image/jpg" });
+  let img = document.createElement("img");
+  img.setAttribute("src", filePath);
+  img.classList.add("sticky-image");
+  addSticky(img);
 });
+
 
 downloadDiv.addEventListener("click" , function(){
     let imagePath = canvas.toDataURL("image/jpg");
