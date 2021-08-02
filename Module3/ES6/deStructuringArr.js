28075512278c08c08d807e5a611e55972370d57c
ujjawaljha <ujjawaljha08@gmail.com>
2021-08-02 20:50:55 +0530

    Arrow function

---
+++ Module3/ES6/deStructuringArr.js
@@ -0,0 +1,9 @@
+let a=[1,2,3,4,5];
+
+let[kuchb,,,reqvar]=a;
+
+console.log(kuchb);
+console.log(reqvar);
+
+//underscore(-)-> is used for skip the variables;
+// lagatar 2 underscore use nhi kar sakte
---
+++ Module3/ES6/deStructuringOBJ.js
@@ -0,0 +1,13 @@
+ let add= {
+    city: "delhi",
+    region: "pitampurra",
+    pin:110001,
+} ;
+
+
+let{city,pin,region,myVar}=add;
+
+console.log(city);
+console.log(pin);
+console.log(region);
+console.log(myVar);
